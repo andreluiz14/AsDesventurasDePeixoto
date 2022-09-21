fef26805507171efd4e284b420b2fd62601cdece
Daniel <danielbatatarpg@hotmail.com>
2022-09-21 15:01:17 -0400

    Estados de animação do Peixe
    Alterações do script de movimento e no animator com o objetivo de adicionar estados de animação no objeto peixe.

--- Assets/Scripts/FishMove.cs
+++ Assets/Scripts/FishMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FishMove : MonoBehaviour
 {
+
+    public Animator animator;
     [SerializeField]
     private Rigidbody2D rb;
 
@@ -11,7 +13,16 @@ public class FishMove : MonoBehaviour
     private float velocidadeMov;
 
 
+
+   
+
     // Update is called once per frame
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Animator anim = GetComponent<Animator>(); 
+    }
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -19,5 +30,10 @@ public class FishMove : MonoBehaviour
 
         Vector2 direcao = new Vector2(horizontal, vertical);
         this.rb.velocity = direcao * this.velocidadeMov;
+
+        animator.SetFloat("horizontal", horizontal);
+        animator.SetFloat("vertical", vertical);
+        animator.SetFloat("Speed", direcao.sqrMagnitude);
+
     }
 }
