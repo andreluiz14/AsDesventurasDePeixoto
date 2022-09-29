9a90b89735e6faf6b23bda12a93b797f2a4f5b6d
SofiaP12 <pereirariosofia@gmail.com>
2022-09-28 22:51:46 -0400

    Scritp pegar objetos

--- Assets/Scripts/MoveBox.cs
+++ Assets/Scripts/MoveBox.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class MoveBox : MonoBehaviour
 {
-    private Rigidbody2D _playerRb;
+    public Rigidbody2D playerRb;
     private float _movimentoHorizontal;
     private float _movimentoVertical;
     [SerializeField] private float _playerSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        _playerRb = GetComponent<Rigidbody2D>();
+        playerRb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,7 +24,7 @@ public class MoveBox : MonoBehaviour
         _movimentoHorizontal = Input.GetAxisRaw("Horizontal");
         _movimentoVertical = Input.GetAxisRaw("Vertical");
 
-        _playerRb.AddForce(Vector2.up * _movimentoVertical * _playerSpeed * Time.deltaTime, ForceMode2D.Force);
-        _playerRb.AddForce(Vector2.right * _movimentoHorizontal * _playerSpeed * Time.deltaTime, ForceMode2D.Force);
+        playerRb.AddForce(Vector2.up * _movimentoVertical * _playerSpeed * Time.deltaTime, ForceMode2D.Force);
+        playerRb.AddForce(Vector2.right * _movimentoHorizontal * _playerSpeed * Time.deltaTime, ForceMode2D.Force);
     }
 }
--- Assets/Scripts/PegarObjetos.cs
+++ Assets/Scripts/PegarObjetos.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PegarObjetos : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    MoveBox jogador;
+    private FixedJoint2D objeto;
+    private void Start()
     {
-        
+        objeto = gameObject.GetComponent<FixedJoint2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D outro)
     {
-        
+        if (outro.CompareTag("Player") && Input.GetKey(KeyCode.Space))
+        {
+            print("Colidiu");
+            //jogador.playerRb = jogador.GetComponent<Rigidbody2D>();
+            objeto.connectedBody = jogador.playerRb;
+        }
     }
 }
