5d6f6beb36a8c1f4d17cb0eacd23c26a18404319
SofiaP12 <pereirariosofia@gmail.com>
2022-09-29 22:29:45 -0400

    Pegar objetos enquanto estiver pressionado

--- Assets/Scripts/PegarObjetos.cs
+++ Assets/Scripts/PegarObjetos.cs
@@ -1,35 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PegarObjetos : MonoBehaviour
 {
+
     public MoveBox _jogador;
     private HingeJoint2D _objeto;
-    public bool teste;
-    [SerializeField] private bool _objetoFixado = false;
+    private bool _estaPressionado = false;
+    [SerializeField] public bool _objetoFixado = false;
+    Input_Player _playerInput;
+    private void Awake()
+    {
+        _playerInput = new Input_Player();
+    }
     private void Start()
     {
         _objeto = gameObject.GetComponent<HingeJoint2D>();
         _objeto.enabled = false;
     }
-    private void Update()
+    private void OnEnable()
     {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            teste = true;
-        }
-        if (_objetoFixado)
-        {
-            _objeto.connectedBody = _jogador.playerRb;
-            _objeto.enabled = true;
-        }
+        _playerInput.Enable();
+    }
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+
+    }
+    public void Interacao(InputAction.CallbackContext context)
+    {
+        _estaPressionado = context.performed;
     }
     private void OnTriggerStay2D(Collider2D outro)
     {
-        if (outro.CompareTag("Player") && Input.GetKeyDown(KeyCode.E))
+        if (outro.gameObject.CompareTag("Player") && _estaPressionado)
         {
-            _objetoFixado = !_objetoFixado;
+            _objeto.enabled = true;
+            _objeto.connectedBody = _jogador.playerRb;
+        }
+        else
+        {
+            _objeto.enabled = false;
+            //_objeto.connectedBody = null;
         }
     }
     private IEnumerator DelayDaFixacao()
